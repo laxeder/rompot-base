592463f28d0ddfb665a5b2c8530821daecf4c16f
laxeder <laxederbr@gmail.com>
2023-07-13 12:45:26 -0300

    (remove) index

--- src/interfaces/index.ts
+++
@@ -1,8 +0,0 @@
-export * from "./command";
-
-export * from "./bot/IAuth";
-export * from "./bot/IBot";
-export * from "./chat/IChat";
-export * from "./IClient";
-export * from "./message/IMessage";
-export * from "./user/IUser";
