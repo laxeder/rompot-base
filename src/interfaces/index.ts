9686fa97cae2f4090b4ef2ce20ecc4c4b67b2a92
laxeder <laxederbr@gmail.com>
2023-07-13 14:44:25 -0300

    (add) exports

---
+++ src/interfaces/index.ts
@@ -0,0 +1,6 @@
+export * from "./bot";
+export * from "./chat";
+export * from "./client";
+export * from "./command";
+export * from "./message";
+export * from "./user";
---
+++ src/types/index.ts
@@ -0,0 +1,7 @@
+export * from "./bot";
+export * from "./chat";
+export * from "./client";
+export * from "./command";
+export * from "./connection";
+export * from "./message";
+export * from "./user";
